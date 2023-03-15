705270956ba981c8499be9cf7d7de79ef2a3e925
Chad Edwards <chad@saw.dev>
2023-03-14 20:51:16 -0400

    adding api updates

--- api/getMinecraftStreamers/index.js
+++ api/getMinecraftStreamers/index.js
@@ -1,13 +1,31 @@
+const axios = require("axios");
+
+const clientId = process.env.TWITCH_CLIENT_ID;
+const clientSecret = process.env.TWITCH_CLIENT_SECRET;
+
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+  const accessToken = await getTwitchAccessToken(clientId, clientSecret);
+  const minecraftStreamers = await getActiveMinecraftStreamers(accessToken);
+  context.res = {
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(minecraftStreamers),
+  };
+};
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+async function getTwitchAccessToken(clientId, clientSecret) {
+  const response = await axios.post(
+    `https://id.twitch.tv/oauth2/token?client_id=${clientId}&client_secret=${clientSecret}&grant_type=client_credentials`
+  );
+  return response.data.access_token;
+}
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+async function getActiveMinecraftStreamers(accessToken) {
+  const headers = {
+    "Client-ID": clientId,
+    "Authorization": `Bearer ${accessToken}`,
+  };
+  const response = await axios.get("https://api.twitch.tv/helix/streams?game_id=27471&first=10", {
+    headers,
+  });
+  return response.data.data;
 }
