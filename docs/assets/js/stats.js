60baf28ad2effdd3a694623d0b6e3a4498d2888d
Chad Edwards <chad@saw.dev>
2023-04-24 18:27:06 -0400

    fixing chart

--- docs/assets/js/stats.js
+++ docs/assets/js/stats.js
@@ -123,47 +123,62 @@ async function drawPieChart(chartId, apiUrl, valueTransformFn) {
 }
 
 
-async function drawStackedBarChart(chartId, apiUrl, valueTransformFn) {
-  const chartData = await getAggregatedStats(apiUrl, valueTransformFn);
-  
-    const margin = { top: 30, right: 30, bottom: 70, left: 100 };
-    const width = 500 - margin.left - margin.right;
-    const height = 300 - margin.top - margin.bottom;
-  
-    const x = d3.scaleLinear()
-      .range([0, width])
-      .domain([0, d3.max(chartData, d => d[1])]);
-  
-    const y = d3.scaleBand()
-      .range([height, 0])
-      .domain(chartData.map(d => d[0]))
-      .padding(0.1);
-  
-    const svg = createSvg(chartId, margin, width, height);
-  
-    svg.append("g")
-      .call(d3.axisLeft(y));
-  
-    svg.append("g")
-      .attr("transform", `translate(0,${height})`)
-      .call(d3.axisBottom(x));
-  
-    svg.selectAll(".bar")
-      .data(chartData)
-      .enter()
-      .append("rect")
-      .attr("class", "bar")
-      .attr("y", d => y(d[0]))
-      .attr("height", y.bandwidth())
-      .attr("x", 0)
-      .attr("width", d => x(d[1]))
-      .attr("fill", "#4e73df");
-  }
-  
-  
+async function drawStackedBarChart(chartId, apiUrl) {
+  const chartData = await getIndividualStats(apiUrl);
+  const players = chartData.map(d => d.player);
+  const travelTypes = ['walk_one_cm', 'swim_one_cm', 'fly_one_cm'];
+
+  // Convert distances from centimeters to meters
+  chartData.forEach(d => {
+    d.walk_one_cm /= 100;
+    d.swim_one_cm /= 100;
+    d.fly_one_cm /= 100;
+  });
+
+  const margin = { top: 30, right: 30, bottom: 70, left: 100 };
+  const width = 500 - margin.left - margin.right;
+  const height = 300 - margin.top - margin.bottom;
+
+
+  const x = d3.scaleBand()
+    .domain(players)
+    .range([0, width])
+    .padding(0.1);
+
+  const y = d3.scaleLinear()
+    .domain([0, d3.max(chartData, d => d.walk_one_cm + d.swim_one_cm + d.fly_one_cm)]).nice()
+    .range([height, 0]);
+
+  const color = d3.scaleOrdinal()
+    .domain(travelTypes)
+    .range(['#4e73df', '#1cc88a', '#36b9cc']);
+
+  const svg = createSvg(chartId, margin, width, height);
+
+  svg.append("g")
+    .attr("transform", `translate(0,${height})`)
+    .call(d3.axisBottom(x));
+
+  svg.append("g")
+    .call(d3.axisLeft(y));
+
+  const layer = svg.selectAll(".layer")
+    .data(d3.stack().keys(travelTypes)(chartData))
+    .enter().append("g")
+    .attr("class", "layer")
+    .attr("fill", d => color(d.key));
+
+  layer.selectAll("rect")
+    .data(d => d)
+    .enter().append("rect")
+    .attr("x", d => x(d.data.player))
+    .attr("y", d => y(d[1]))
+    .attr("height", d => y(d[0]) - y(d[1]))
+    .attr("width", x.bandwidth());
+}
 
 // Call the functions for the specific charts
 drawBarChart("chart1", "https://stats.minecrafting.live/playerstats?category=minecraft:crafted&top=10&sort=desc");
 drawBarChart("chart2", "https://stats.minecrafting.live/summarizedstats?statType=animals_bred");
 drawPieChart("chart3", "https://stats.minecrafting.live/summarizedstats?statType=play_time", stats => stats.play_time / 3600); // Convert seconds to hours
-drawStackedBarChart("chart4", "https://stats.minecrafting.live/summarizedstats?statTypes=walk_one_cm,swim_one_cm,fly_one_cm", stats => stats.one_cm / 100); // Convert centimeters to meters
+drawStackedBarChart("chart4", "https://stats.minecrafting.live/summarizedstats?statTypes=one_cm"); // Convert centimeters to meters
