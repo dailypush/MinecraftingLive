0e26f2991bfa955c2d64935fd952e564dffeb009
Chad Edwards <chad@saw.dev>
2023-04-25 16:27:48 -0400

    have 2 drawpies we only need one

--- docs/assets/js/charts.js
+++ docs/assets/js/charts.js
@@ -133,40 +133,6 @@ async function drawPieChart(chartId, apiUrl) {
     });
 }
 
-  async function drawPieChart(chartId, apiUrl) {
-    const data = await fetchData(apiUrl);
-    const isArray = Array.isArray(data);
-    const labels = isArray ? data.map(item => item.player) : Object.keys(data);
-    const values = isArray ? data.map(item => item.value) : Object.values(data);
-
-    const ctx = document.getElementById(chartId).getContext('2d');
-    new Chart(ctx, {
-        type: 'pie',
-        data: {
-            labels: labels,
-            datasets: [{
-                data: values,
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        }
-    });
-}
 
 async function getIndividualStats(apiUrl) {
     const data = await fetchData(apiUrl);
